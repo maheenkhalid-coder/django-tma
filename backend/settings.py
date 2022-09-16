3a3fa4ff8ff306a4311c68caf04bdf3c96059de4
Maheen Khalid <maheenkhalid652@gmail.com>
2022-09-16 18:07:24 +0500

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -127,12 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
